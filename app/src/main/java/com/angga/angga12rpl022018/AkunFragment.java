148819fdd3c801d21aa72327fde3e3b3f3af8f6c
Your Name <you@example.com>
2020-08-24 01:27:20 +0700

    Dashboard

--- app/src/main/java/com/angga/angga12rpl022018/AkunFragment.java
+++ app/src/main/java/com/angga/angga12rpl022018/AkunFragment.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class AkunFragment extends Fragment {
+    Button btnlogout;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
